aaee8802142da630260c21e8ba07e6be39e67f0b
tanwairahul <tanwanirahul@gmail.com>
2014-05-03 10:55:59 +0530

    Adding solution for first question - reverse the sentence

---
+++ reverse.py
@@ -0,0 +1,24 @@
+'''
+Created on 03-May-2014
+
+@author: rahul
+'''
+import re
+
+
+def reverse(sentence):
+    '''
+        Returns a reversed sentence.
+    '''
+    def string_reverse(matchobj):
+        '''
+            Returns reversed string.
+        '''
+        string_data = matchobj.group(0)
+        return string_data[::-1]
+
+    return re.sub("([\w-])+", string_reverse, sentence)
+
+if __name__ == "__main__":
+    sentence = raw_input("Sentence to reverse: ")
+    print "Reversed sentence : ", reverse(sentence)
